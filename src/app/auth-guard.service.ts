9b052031b67f31c0e46ad23a9f142baad52f96d0
christianarredondo <christianarredondo@utexas.edu>
2017-07-30 00:55:31 -0500

    conclude routing exercise

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,5 +1,6 @@
 import {
   CanActivate,
+  CanActivateChild,
   ActivatedRouteSnapshot,
   Router,
   RouterStateSnapshot
@@ -9,23 +10,28 @@ import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
     constructor(
       private authService: AuthService,
       private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot,
               state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-      return this.authService.isAuthenticated()
-          .then(
-              (authenticated: boolean) => {
-                  if (authenticated) {
-                      return true;
-                  } else {
-                      this.router.navigate(['/']);
-                  }
-              }
-          );
+    return this.authService.isAuthenticated()
+        .then(
+            (authenticated: boolean) => {
+                if (authenticated) {
+                    return true;
+                } else {
+                    this.router.navigate(['/']);
+                }
+            }
+        );
+  }
+
+  canActivateChild(route: ActivatedRouteSnapshot,
+              state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    return this.canActivate(route, state);
   }
 }
 
